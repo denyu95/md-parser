badf2417f9cb0c082d1dfd5a8e38182513aff7e8
denyu95 <912078294@qq.com>
2019-01-18 18:16:34 +0800

    新增代码块语法解析&段落语法解析

--- md_parser.js
+++ md_parser.js
@@ -11,8 +11,29 @@ function Lexer() {
 // 词法分析器核心
 Lexer.rules = {
     newline: /^\n+/,
-    heading: /^ *(#{1,6}) +([^\n]+?)(?:\n+|$)/
+    heading: /^ *(#{1,6}) +([^\n]+?)(?:\n+|$)/,
+    codeblock: /^ *`{3} *\n{1}([\S\s]*?)\n{0,1}`{3}/,
+    paragraph: /^[^\n]+(?:\n(?!heading|codeblock)[^\n]+)*/
 };
+
+Lexer.rules.paragraph = edit(Lexer.rules.paragraph)
+.replace('heading',' *#{1,6} +[^\\n]+')
+.replace('codeblock',' *`{3} *\\n{1}[\\S\\s]*?\\n`{3}')
+.getRegex();
+
+function edit(regex) {
+    regex = regex.source || regex;
+    return {
+        replace: function(name, val) {
+            val = val.source || val;
+            regex = regex.replace(name, val);
+            return this;
+        },
+        getRegex: function() {
+            return new RegExp(regex);
+        }
+    };
+}
 
 // 词法分析器静态函数
 Lexer.lex = function(src) {
@@ -33,6 +54,20 @@ Lexer.prototype.lex = function(src) {
                 level: cap[1].length,
                 text: cap[2]
             });
+            continue;
+        } else if(cap = Lexer.rules.codeblock.exec(src)) {
+            src = src.substring(cap[0].length);
+            this.tokens.push({
+                type: 'codeblock',
+                text: cap[1]
+            });
+            continue;
+        } else if(cap = Lexer.rules.paragraph.exec(src)) {
+            src = src.substring(cap[0].length);
+            this.tokens.push({
+                type: 'paragraph',
+                text: cap[0]
+            })
             continue;
         } else {
             src = '';
@@ -58,7 +93,16 @@ Parser.prototype.parse = function(tokens) {
     tokens.forEach(token => {
         switch(token.type) {
             case 'heading': {
-                this.out += '<h' + token.level + '>' + token.text + '</h' + token.level +'>';
+                this.out += '<h' + token.level + '>' + token.text + '</h' + token.level +'>\n';
+                break;
+            }
+            case 'codeblock': {
+                this.out += '<pre><code>' + token.text + '</code></pre>\n';
+                break;
+            }
+            case 'paragraph': {
+                this.out += '<p>' + token.text + '</p>\n';
+                break;
             }
         }
     });
